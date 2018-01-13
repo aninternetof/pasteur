663b011b145434979df19df7e1dab9bebc767f13
Brady L. Hurlburt <bradyhurlburt@gmail.com>
2018-01-13 12:54:43 -0500

    Add key auth to API

--- pasteur/controllers/main.py
+++ pasteur/controllers/main.py
@@ -1,4 +1,7 @@
 import os
+import random
+import string
+
 import netifaces as ni
 from datetime import datetime
 from flask import Blueprint, request, redirect, url_for, jsonify
@@ -10,6 +13,7 @@ from pasteur.models import db, User, Run
 
 main = Blueprint('main', __name__)
 
+api_keys = {}
 
 @main.route('/api/v1')
 @cache.cached(timeout=1000)
@@ -17,12 +21,29 @@ def home():
     return "Pasteur API root V1."
 
 
+def _verify_api_key(request):
+    try:
+        user_id = request.get_json()['user_id']
+        api_key = request.get_json()['api_key']
+    except KeyError:
+        return False
+    if user_id in api_keys and api_key in api_keys[user_id]:
+        return True
+    else:
+        return False
+
+
 @main.route('/api/v1/run', methods=["GET","POST"])
 def run_view():
     if request.method == 'GET':
         return "Not implemented yet", 501
     if request.method == 'POST':
-        name = request.get_json()['name']
+        if not _verify_api_key(request):
+            return "Permission to API denied", 405
+        try:
+            name = request.get_json()['name']
+        except KeyError:
+            return "Bad message", 400
         if 'root' in os.path.expanduser('~'):
             log_file_path = '/var/log/{}_name'.format(datetime.now().strftime("%Y_%m_%d_%H_%M_%S"), name)
         else:
@@ -53,6 +74,9 @@ def sys_info_view():
 @main.route('/api/v1/enabled', methods=["GET", "POST"])
 def enabled_view():
     if request.method == 'POST':
+        if not _verify_api_key(request):
+            return "Permission to API denied", 405
+        if 'value' not in request.get_json(): return "Bad message", 400
         value = request.get_json()['value']
         thermostat.attributes['enabled'] = value
         if not value:
@@ -68,6 +92,9 @@ def enabled_view():
 @main.route('/api/v1/target-temp-degc', methods=["GET", "POST"])
 def target_tempc_view():
     if request.method == 'POST':
+        if not _verify_api_key(request):
+            return "Permission to API denied", 405
+        if 'value' not in request.get_json(): return "Bad message", 400
         value = request.get_json()['value']
         try:
             thermostat.attributes['target_temp_degc'] = float(value)
@@ -82,6 +109,9 @@ def target_tempc_view():
 @main.route('/api/v1/period_s', methods=["GET", "POST"])
 def period_s_view():
     if request.method == 'POST':
+        if not _verify_api_key(request):
+            return "Permission to API denied", 405
+        if 'value' not in request.get_json(): return "Bad message", 400
         value = request.get_json()['value']
         try:
             thermostat.attributes['period_s'] = float(value)
@@ -96,6 +126,9 @@ def period_s_view():
 @main.route('/api/v1/target-degc-minutes', methods=["GET", "POST"])
 def target_degc_minutes_view():
     if request.method == 'POST':
+        if not _verify_api_key(request):
+            return "Permission to API denied", 405
+        if 'value' not in request.get_json(): return "Bad message", 400
         value = request.get_json()['value']
         try:
             thermostat.attributes['target_degc_minutes'] = float(value)
@@ -110,6 +143,9 @@ def target_degc_minutes_view():
 @main.route('/api/v1/bottom-margin-degc', methods=["GET", "POST"])
 def bottom_margin_degc_view():
     if request.method == 'POST':
+        if not _verify_api_key(request):
+            return "Permission to API denied", 405
+        if 'value' not in request.get_json(): return "Bad message", 400
         value = request.get_json()['value']
         try:
             thermostat.attributes['bottom_margin_degc'] = float(value)
@@ -124,6 +160,9 @@ def bottom_margin_degc_view():
 @main.route('/api/v1/top-margin-degc', methods=["GET", "POST"])
 def top_margin_degc_view():
     if request.method == 'POST':
+        if not _verify_api_key(request):
+            return "Permission to API denied", 405
+        if 'value' not in request.get_json(): return "Bad message", 400
         value = request.get_json()['value']
         try:
             thermostat.attributes['top_margin_degc'] = float(value)
@@ -135,21 +174,22 @@ def top_margin_degc_view():
         return jsonify({'value': thermostat.attributes['top_margin_degc']})
 
 
-@main.route("/login", methods=["GET", "POST"])
-def login():
-    form = LoginForm()
+@main.route("/get-api-key", methods=["POST"])
+def api_key_view():
+    if 'username' not in request.get_json(): return "Bad message", 400
+    username = request.get_json()['username']
+    if 'password' not in request.get_json(): return "Bad message", 400
+    password = request.get_json()['password']
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return "Invalid credentials", 405
+    if user.check_password(password):
+        key = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(24))
+        if user.id not in api_keys:
+            api_keys[user.id] = [key]
+        else:
+            api_keys[user.id].append(key)
+        return jsonify({'user_id': user.id, 'api_key': key})
+    else:
+        return "Invalid credentials", 405
 
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).one()
-        login_user(user)
-
-        return redirect(request.args.get("next") or url_for(".home"))
-
-    return "Login success."
-
-
-@main.route("/logout")
-def logout():
-    logout_user()
-
-    return "Logout success."
