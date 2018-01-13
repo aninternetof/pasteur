afae64d671802e3af4b17975503ccf28ca371569
Brady L. Hurlburt <bradyhurlburt@gmail.com>
2018-01-12 22:24:06 -0500

    Add gpio control for pump

--- pasteur/tasks/thermostat.py
+++ pasteur/tasks/thermostat.py
@@ -2,18 +2,29 @@ import random
 from flask import json
 from datetime import datetime
 
+try:
+    from gpiozero import LED
+except Exception:
+    print("Failed to load gpiozero. But not be on Raspberry Pi")
+
+
+
 class Thermostat:
 
     def __init__(self, socketio):
         self.socketio = socketio
+        try:
+            self.pump = LED(21)
+        except NameError:
+            pass
         self.attributes = {
             'temp_reading_degc': -1,
             'period': 5,
-            'target_temp_degc': -1,
+            'target_temp_degc': 50,
             'target_degc_sec': -1,
             'top_margin_degc': 1,
             'bottom_margin_degc': 1,
-            'enabled': False,
+            'enabled': True,
             'pump_on': False,
             'timestamp': None,
             'run_name': '',
@@ -30,11 +41,18 @@ class Thermostat:
                 a = self.attributes
                 # TODO: a[temp_reading_degc] =
                 if (a['temp_reading_degc'] < (a['target_temp_degc'] - a['bottom_margin_degc'])):
-                    # TODO: turn on pump
+                    try:
+                        self.pump.on();
+                    except AttributeError:
+                        print("Not on Raspberry Pi. Cannot turn pump on.")
                     a['pump_on'] = True
-                elif (a['temp_reading_degc'] < (a['target_temp_degc'] - a['bottom_margin_degc'])):
-                    # TODO: turn off pump
+                elif (a['temp_reading_degc'] < (a['target_temp_degc'] + a['bottom_margin_degc'])):
+                    try:
+                        self.pump.off();
+                    except AttributeError:
+                        print("Not on Raspberry Pi. Cannot turn pump off.")
                     a['pump_on'] = False
                 with open(self.attributes['log_file_path'], 'a') as f:
                     f.write(json.dumps(self.attributes)+'\n')
+                self.socketio.emit('log', json.dumps(self.attributes))
             self.socketio.sleep(self.attributes['period'])
